25fb96569041e7b833999f4f83fc259c32043432
insecurity22 <insecurity22@naver.com>
2026-06-26 17:20:18 +0900

    feat: add component scanning and dependency injection

--- src/main/java/board/board_spring/repository/MemberRepository.java
+++ src/main/java/board/board_spring/repository/MemberRepository.java
@@ -1,10 +1,12 @@
 package board.board_spring.repository;
 
 import board.board_spring.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface MemberRepository {
     Member save(Member member);
     Optional<Member> findById(Long id);
--- src/main/java/board/board_spring/service/MemberService.java
+++ src/main/java/board/board_spring/service/MemberService.java
@@ -2,7 +2,8 @@ package board.board_spring.service;
 
 import board.board_spring.domain.Member;
 import board.board_spring.repository.MemberRepository;
-import board.board_spring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,10 +11,12 @@ import java.util.Optional;
 // 회원 서비스 클래스
 // - 서비스의 역할은 실제 비즈니스 로직을 처리하는 것이다.
 // - 따라서 클래스명과 메서드명은 기술적인 용어보다 비즈니스 도메인에 가까운 용어를 사용하는 것이 좋다.
+@Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
